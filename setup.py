eae51447b2c7ae5c2d46abd6c121c042b43454ff
Waylon Walker <walker_waylon_s@cat.com>
2021-06-18 08:02:51 -0500

    Add setup.py packaging script

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from pathlib import Path
+
+requires = Path("requirements.txt").read_text().split()
+
+setup(
+    name="pypi_list",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=requires,
+    entry_points={"console_scripts": ["pypi-list = pypi_list:main"]},
+)
