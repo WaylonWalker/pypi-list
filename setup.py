1270d868431a254834734411255660b5942ec99c
Waylon Walker <walker_waylon_s@cat.com>
2021-06-18 08:21:03 -0500

    implement requirments_dev.txt

--- setup.py
+++ setup.py
@@ -2,11 +2,17 @@ from setuptools import setup, find_packages
 from pathlib import Path
 
 requires = Path("requirements.txt").read_text().split()
+dev_requires = Path("requirements_dev.txt").read_text().split()
 
 setup(
     name="pypi_list",
     version="0.1.0",
     packages=find_packages(),
     install_requires=requires,
+    extras_require={
+        "all": [*requires, *dev_requires],
+        "dev": dev_requires,
+        "prod": requires,
+    },
     entry_points={"console_scripts": ["pypi-list = pypi_list:main"]},
 )
